25a7b2ee5dc26244faaf82bb4f9adcdfd91e41d5
Whithman <144461701+Whithman@users.noreply.github.com>
2026-09-04 18:13:47 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4510,7 +4510,7 @@ $seoImage = (!empty($storeSettings['shop_logo']) && strpos($storeSettings['shop_
     <?php endif; ?>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&family=Poppins:ital,wght@0,400;0,500;0,600;0,700;0,800;1,400&display=swap" rel="stylesheet" />
     <!-- JsBarcode - linear barcode generator -->
-    <!-- JsBarcode v3.11.6, inlined (not loaded from CDN) so barcode
+    <!-- JsBarcode v3.11.6, inlined (not loaded from CDN) so barcode -->
     <script>
         /*! JsBarcode v3.11.6 | (c) Johan Lindell | MIT license */ ! function(t) {
             var e = {};
